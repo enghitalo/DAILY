b2620cd906109ee61ae37666db0482283c3dfa43
enghitalo <enghitalo@gmail.com>
2022-09-26 14:01:02 -0300

    user entity

---
+++ typescript/src/user.entities.ts
@@ -0,0 +1,10 @@
+export type User = {
+  id: number;
+  username: string;
+  password: string;
+  name: string;
+  created_at: string;
+  updated_at: string;
+  deleted_at: string;
+  active: boolean;
+};
